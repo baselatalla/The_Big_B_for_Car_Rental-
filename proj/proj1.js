dd94c3c9e8e89d745bb3db64894f1ca6fa901db9
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-02-24 20:24:04 +0200

    using fuction & loops

--- proj/proj1.js
+++ proj/proj1.js
@@ -1,26 +1,39 @@
 confirm("Are you ready to start");
 
 var name1 = prompt("Inter Your Name :");
-var birthdate = prompt("Inter Your birth date :");
-var weight = prompt("Inter Your Weight ( in Kg) :");
-var tall = prompt("Inter Your Tall (in cm Ex = 172 ) :");
-
+var birthdate = prompt("Inter Your birth year Ex=(1990) :");
 var intage=parseInt(birthdate);
-var intweight=parseInt(weight);
-var inttall=parseInt(tall);
 
-if(intage < 2022 ){
+while( 1900 > intage || intage > 2021 || isNaN(intage) ){
+    alert("your birth date is not valid") ;
+    console.log("your birth date is not valid");
+var birthdate = prompt("Inter Your birth year Ex=(1990) :");
+var intage=parseInt(birthdate);
+}
+
+if(1900 <intage || intage < 2021 ){
     var age = 2021 - intage ;
     console.log("your birth date is valid");
-} else {console.log("your birth date is not valid") ;
-alert("your birth date is not valid") ;
+}  
+
+
+var weight = prompt("Inter Your Weight ( in Kg) :");
+var tall = prompt("Inter Your Tall (in cm Ex = 172 ) :");
+var reImage = prompt(" how many glasse of wtare did you drink a day?")
+var intweight=parseInt(weight);
+var inttall=parseInt(tall);
+var intreimage=parseInt(reImage);
+// print images using for loop
+var counter =0 ;
+var g = intreimage ;
+for (var i =1 ; i <= g ; i++){
+    counter++;
+    document.write('<img src="./images/water.jpg" alt="">' + '<h3>'+ counter + '</h3>');
+
 }
-console.log(intweight); 
-console.log(inttall);
 var x = intweight;
 var y = inttall;
-console.log(x); 
-console.log(y);
+
 var gh =" in the healthy rang";
 var ow =" overweight";
 var uw =" underweight";
@@ -29,9 +42,10 @@ if(18 <= age && age <=65){
     var bmi =(x/(y ** 2))*10000; 
     console.log("your age are applicable to the BMI");
     console.log(bmi);
-} else{ console.log("your age are not applicable to the BMI");
-          var yourhealth ="You are under the age ";
-          alert("your birth date is not valid") ;
+} else{ 
+    console.log("your age are not applicable to the BMI");
+         if (18 > age > 0) {var yourhealth ="You are under the age ";}
+          console.log(" No BMI for under 18") ;
 }
 
 if( 18.5 <= bmi && bmi <= 24.9 ){
@@ -55,7 +69,16 @@ if( 18.5 <= bmi && bmi <= 24.9 ){
             }
         }
     }
-console.log(message);
+ // how much water needed according to your mass  using the function
+ // water ( in liters) = weight (in kg) * .033
+  function waterCal(g,intweight){
+  var waterMustDrink = intweight * 0.033;
+  var waterYouDrink = g * .3;
+  var waterNeeded = waterMustDrink - waterYouDrink;
+  alert("You have to drink more water by : " + waterNeeded+'L' )
+
+  } waterCal( g, intweight);
+
 console.log(bmi);
 var element = document.getElementById("yourage");
 element.innerText = name1 + "  Your Age is : " + age;
